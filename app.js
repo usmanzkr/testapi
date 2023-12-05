c4cb45abce8685889e32057f8ede53c48f0ccc3e
usmanzkr <usmanzakaria125@gmail.com>
2023-12-05 20:24:34 +0700

    Commit with .env

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const app = express();
 const port =  process.env.PORT ||3000;
 const secretKey = 'your-secret-key';
 
-configDotenv
-require('dotenv').config()
-
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
